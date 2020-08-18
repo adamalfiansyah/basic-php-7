829e6da15746fd5755fca97869fefc34a67146ef
Adam Alfiansyah <adamalfiansyah@gmail.com>
2020-08-18 19:24:47 +0700

    array dengan key

---
+++ array-dengan-key.php
@@ -0,0 +1,15 @@
+<?php
+    
+    $buah = [
+        'a' => 'apel', 
+        'm' => 'mangga', 
+        'n' => 'nanas'
+    ];
+
+    print_r($buah);
+
+    echo "<br/>"; //br adalah tag html untuk berpindah baris
+
+    echo $buah['n'];
+
+?>
