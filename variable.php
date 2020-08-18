266123e5d2489782eafa64d51176792ab50e7953
Adam Alfiansyah <adamalfiansyah@gmail.com>
2020-08-18 19:23:35 +0700

    variable

---
+++ variable.php
@@ -0,0 +1,20 @@
+<?php
+
+    $nama = 'Adam';
+    echo $nama;
+
+    $alamat = 'Jl Padjajaran';
+    echo $alamat;
+
+    $makanan_kesukaan = 'Ayam goreng';
+    echo $makanan_kesukaan;
+
+    //false (salah)
+    // $makanan-kesukaan = 'Ayam goreng';
+    // echo $makanan-kesukaan;
+
+    //false
+    // $2nama = 'Alfiansyah';
+    // echo $2nama;
+
+?>
