606451cf4bf0dd332719a4e958ab1fa248763bba
Adam Alfiansyah <adamalfiansyah@gmail.com>
2020-08-18 19:29:31 +0700

    multidimensi array

---
+++ multidimensi-array.php
@@ -0,0 +1,16 @@
+<?php
+
+    $siswa = [
+        'pria'   => ['Anwar', 'Joko', 'Abdi'],
+        'wanita' => ['Desi', 'Putri', 'Monika']
+    ];
+
+    print_r($siswa);
+
+    echo "<br/>";
+    echo $siswa['pria'][1];
+
+    echo "<br/>";
+    echo $siswa['wanita'][2];
+
+?>
